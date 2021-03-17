5e0d8321ff53b1ad3da20da9a23d9ec88dc5122a
Matias Leez <matiasl@kefsensei.com>
2021-03-17 11:01:15 -0300

    check unity test

--- unityPlayTest/Assets/Tests/TestPlayMode.cs
+++ unityPlayTest/Assets/Tests/TestPlayMode.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace Tests
@@ -18,12 +19,19 @@ namespace Tests
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator TestPlayModeWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            Debug.Log("Start Test");
+        public IEnumerator TestPlayModeWithEnumeratorPasses() {
             yield return new WaitForSeconds(1);
+            SceneManager.LoadScene("SampleScene");
+            yield return new WaitForSeconds(1);
+            Debug.Log("Start Test");
+            int time = 0;
+            while ((time < 100)) {
+                yield return new WaitForSeconds(10);
+                time += 10;
+                Debug.Log($"Testing time:{time}s");
+            }
+            
+
             Debug.Log("Start Test End");
             yield return null;
         }
